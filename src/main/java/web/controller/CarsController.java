d033b022cfb64235524dd3aecd539a8b0530beff
Seawitcher <Seawitcher83@yandex.ru>
2022-11-13 14:23:44 +0400

    Version 0.5

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -3,8 +3,14 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 
@@ -36,4 +42,21 @@ public class CarsController {
         model.addAttribute("car", car);
         return "car-info";
     }
+
+    @PostMapping
+    public String newCar (@ModelAttribute("car") Car car) {
+        carService.add(car);
+        return "redirect:/";
+    }
+    @DeleteMapping("delete/{id}")
+    public String deleteCar(@PathVariable("id") int id) {
+        carService.deleteCar(id);
+        return "redirect:/";
+    }
+    @PatchMapping("{id}")
+    public String carUpdate(@PathVariable("id") int id, @ModelAttribute("car") Car car) {
+        carService.updateCar(car);
+        return "redirect:/";
+    }
+
 }
