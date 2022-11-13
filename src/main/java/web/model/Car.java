a41defeb73c2ea78769ed8ffe1bd60f705bbf6ac
Seawitcher <Seawitcher83@yandex.ru>
2022-11-13 11:04:28 +0400

    Version 0.1

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,10 +1,22 @@
 package web.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
-
+@Entity
+@Table(name = "cars")
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+    @Column(name = "model")
     private String modelCar;
+    @Column(name = "color")
     private String colorCar;
 
 
